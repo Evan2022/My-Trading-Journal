7c010a0754500679a325e9db5c8bad11d3f16781
Evan2022 <evan201198@hotmail.com>
2022-10-24 16:48:18 +0000

    create TradeView url

--- trades/urls.py
+++ trades/urls.py
@@ -8,5 +8,7 @@ urlpatterns = [
     path('', views.HomeView.as_view(), name='home'),
     path('journals/', views.JournalListView.as_view(), name='journals'),
     path('journals/add/', views.JournalCreateView.as_view(), name='add_journal'),
+    path('journals/<int:pk>', TradeView.as_view(), name='trade_view')
+
 ]
 
