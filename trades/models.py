b42562aa025f003c958dbd643cd48ce3ae97524b
Evan2022 <evan201198@hotmail.com>
2022-10-24 16:52:58 +0000

    create a get url to select individual journals by their pk

--- trades/models.py
+++ trades/models.py
@@ -1,11 +1,13 @@
 from django.db import models
-from django.db import models
+from django.db import reverse
 
 
-# Create your models here.
 class Journal(models.Model):
 
     name = models.CharField(null=False, blank=False, max_length=255)
+
+    def get_absolute_url(self):
+        return reverse('trades:trade_view', kwargs={'pk': self.pk})
 
     def __str__(self):
         return self.name
