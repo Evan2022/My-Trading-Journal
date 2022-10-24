6c9ea0c48a7a49a3f038fd2dde69222dc94ceda9
Evan2022 <evan201198@hotmail.com>
2022-10-24 12:35:34 +0000

    Create basic Trade and Journal models

--- trades/models.py
+++ trades/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.db import models
+
 
 # Create your models here.
+class Journal(models.Model):
+
+    name = models.CharField(null=False, blank=False, max_length=255)
+
+    def __str__(self):
+        return self.name
+
+
+class Trade(models.Model):
+
+    title = models.CharField(null=False, blank=False, max_length=255)
+
+    journal = models.ForeignKey('Journal', null=False, blank=False, on_delete=models.CASCADE, related_name='trade_journal')
+
+    
